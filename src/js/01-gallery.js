b74f194531807c89ec707323cbefe8e61d147e94
Ivan-Tkachuk <tkachuchella@gmail.com>
2022-12-14 15:19:01 +0200

    testone

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const hello = 'hello';
+console.log(hello);
 console.log(galleryItems);
