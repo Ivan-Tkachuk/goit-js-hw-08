09c317b2fa2106bd777b874c8c4590cfd69a52b2
Ivan-Tkachuk <tkachuchella@gmail.com>
2022-12-16 22:48:09 +0200

    hm-08-jh

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,16 +5,10 @@ const vimeo = document.querySelector('#vimeo-player');
 
 const player = new Player(vimeo);
 
-const _ = require('lodash.throttle');
-
-const throttled = throttle(foo, 1000);
-
-player.on('timeupdate', throttled);
-
-function foo({ seconds }) {
+player.on('timeupdate', throttle(function({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
   console.log(seconds);
-}
+}, 1000));
 
 player
   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
